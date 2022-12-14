b1ac70ce94678c8bfd026073947cc3f82b5bac9b
HayHay <dev@hayhay.dev>
2022-12-13 22:02:22 -0500

    Begin adding coords system

--- src/deploy-commands.ts
+++ src/deploy-commands.ts
@@ -3,41 +3,72 @@ import { REST } from "@discordjs/rest";
 import { config } from "dotenv";
 
 const commands = [
-    new SlashCommandBuilder()
-        .setName("ping")
-        .setDescription("Replies with Pong!"),
-    new SlashCommandBuilder()
-        .setName("link")
-        .setDescription("Sends an email to your scarletmail with a code to input")
-        .addStringOption((option) =>
-            option
-                .setName("netid")
-                .setDescription("Your netid")
-                .setRequired(false)
-        )
-        .addStringOption((option) =>
-            option
-                .setName("code")
-                .setDescription("The code sent to your email")
-                .setRequired(false)
-        ),
-    new SlashCommandBuilder()
-        .setName("minecraft")
-        .setDescription("Links your minecraft username to your netid")
-        .addStringOption((option) =>
-            option
-                .setName("username")
-                .setDescription("Your minecraft username")
-                .setRequired(true)
+  new SlashCommandBuilder()
+    .setName("ping")
+    .setDescription("Replies with Pong!"),
+  new SlashCommandBuilder()
+    .setName("link")
+    .setDescription("Sends an email to your scarletmail with a code to input")
+    .addStringOption((option) =>
+      option.setName("netid").setDescription("Your netid").setRequired(false)
+    )
+    .addStringOption((option) =>
+      option
+        .setName("code")
+        .setDescription("The code sent to your email")
+        .setRequired(false)
     ),
-    new SlashCommandBuilder()
-        .setName("optout")
-        .setDescription("Remove all your data from the database"),
-    
-    new SlashCommandBuilder()
-        .setName("whois")
-        .setDescription("Get a user's netid and minecraft username")
-    
+  new SlashCommandBuilder()
+    .setName("minecraft")
+    .setDescription("Links your minecraft username to your netid")
+    .addStringOption((option) =>
+      option
+        .setName("username")
+        .setDescription("Your minecraft username")
+        .setRequired(true)
+    ),
+  new SlashCommandBuilder()
+    .setName("optout")
+    .setDescription("Remove all your data from the database"),
+
+  new SlashCommandBuilder()
+    .setName("whois")
+    .setDescription("Get a user's netid and minecraft username")
+    .addStringOption((option) =>
+      option
+        .setName("netid")
+        .setDescription("Gets a user based off their netid")
+    )
+    .addStringOption((option) =>
+      option
+        .setName("username")
+        .setDescription("Gets a user based off their minecraft username")
+    )
+    .addMentionableOption((option) =>
+      option
+        .setName("discord")
+        .setDescription("Gets a user based off their Discord username")
+    ),
+
+  new SlashCommandBuilder()
+    .setName("coords-set")
+    .setDescription("Set your coords")
+    .addStringOption((option) =>
+      option
+        .setName("coords")
+        .setDescription("Your coords")
+        .setRequired(false)
+    ),
+  
+  new SlashCommandBuilder()
+    .setName("coords-get")
+    .setDescription("Get your coords")
+    .addUserOption((option) =>
+      option
+        .setName("user")
+        .setDescription("The user to get coords for")
+        .setRequired(false)
+    ),
 ];
 
 config();
