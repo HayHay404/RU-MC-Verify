7c41ed1c29191be64dc036d08f00661ca5bfa111
HayHay <dev@hayhay.dev>
2022-12-23 20:58:40 -0500

    Presence update w/ players (untested)

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,11 @@
 import { config } from "dotenv";
-import { ChatInputCommandInteraction, Client, Interaction, StringSelectMenuInteraction } from "discord.js";
+import {
+  ActivityType,
+  ChatInputCommandInteraction,
+  Client,
+  Interaction,
+  StringSelectMenuInteraction,
+} from "discord.js";
 import { PrismaClient } from "@prisma/client";
 import { whois } from "./commands/moderator/whois";
 import { getCoords } from "./commands/coords/getCoords";
@@ -7,6 +13,7 @@ import { setCoords } from "./commands/coords/setCoords";
 import { setIGN } from "./commands/link/setIGN";
 import { deleteCoords } from "./commands/coords/delCoords";
 import { serverInfo } from "./commands/server/serverInfo";
+import { mcStatus } from "./scripts/mcStatus";
 
 config(); // Set up dotenv config and get it from root of project
 
@@ -18,47 +25,81 @@ const client = new Client({ intents: [] }); // Initalize new client, doesn't nee
   await client.login(process.env.TOKEN);
 })(); // Use the token from .env file to login to the bot
 
+// create a presence and refresh it every 5 minutes with the player count
 client.on("ready", () => {
   console.log("Bot is ready!");
+});
+
+// client.on("guildCreate", (guild) => {
+//   db.discordServer.upsert({
+//     where: {
+//       id: guild.id,
+//     },
+//     update: {
+//       id: guild.id,
+//     },
+//     create: {
+//       id: guild.id,
+//     },
+// });
+
+async function setPresence() {
+  client.user?.setPresence({
+    activities: [
+      {
+        name: `with ${(await mcStatus()).players.online} players`,
+        type: ActivityType.Playing,
+      },
+    ],
+    status: "online",
+  });
+}
+
+client.on("ready", async () => {
+  await setPresence();
+  setInterval(async () => {
+    await setPresence();
+  }, 300000);
 });
 
 client.on("interactionCreate", async (interaction: Interaction) => {
   // Check if the interaction is a command and then reply if the command is "ping"
   if (!interaction.isCommand()) return; // If the interaction isn't a command, return (stop the function
 
-    const command = interaction.commandName; // Get the command name from the interaction
+  const command = interaction.commandName; // Get the command name from the interaction
 
-    if (command === "ping") {
-      await interaction.reply("pong");
-    }
+  if (command === "ping") {
+    await interaction.reply("pong");
+  }
 
-    console.log("Command: " + command);
-    switch (command) {
-      case "whois":
-        whois(interaction as ChatInputCommandInteraction);
-        break;
-      case "minecraft":
-        setIGN(interaction as ChatInputCommandInteraction);
-        break;
-      case "server":
-        serverInfo(interaction as ChatInputCommandInteraction);
-        break;
-      case "ip":
-        serverInfo(interaction as ChatInputCommandInteraction);
-        break;
-      case "coords-set":
-        setCoords(interaction as ChatInputCommandInteraction);
-        break;
-      case "coords-get":
-        getCoords(interaction as ChatInputCommandInteraction);
-        break;
-      case "coords-delete":
-        deleteCoords(interaction as ChatInputCommandInteraction);
-        break;
-      
-      default:
-        break;
-    }
+  switch (command) {
+    case "whois":
+      whois(interaction as ChatInputCommandInteraction);
+      break;
+    case "minecraft":
+      setIGN(interaction as ChatInputCommandInteraction);
+      break;
+    case "server":
+      serverInfo(interaction as ChatInputCommandInteraction);
+      break;
+    case "ip":
+      serverInfo(interaction as ChatInputCommandInteraction);
+      break;
+    case "coords-set":
+      setCoords(interaction as ChatInputCommandInteraction);
+      break;
+    case "coords-get":
+      getCoords(interaction as ChatInputCommandInteraction);
+      break;
+    case "coords-delete":
+      deleteCoords(interaction as ChatInputCommandInteraction);
+      break;
+    case "info":
+      break;
+
+    default:
+      break;
+  }
 });
 
 // client.on("interactionCreate", async (interaction: Interaction) => {
@@ -70,7 +111,7 @@ client.on("interactionCreate", async (interaction: Interaction) => {
 //     case "deleteCoords":/
 //       deleteCoords(interaction as StringSelectMenuInteraction);
 //       break;
-  
+
 //     default:
 //       break;
 //   }
